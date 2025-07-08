7492e83636bc724857031802e72939b56d8f2ea4
TalonXDD <nthu110062210@gapp.nthu.edu.tw>
2025-07-08 20:54:03 +0800

    camera follow player

--- assets/Scripts/Others/camera.ts
+++ assets/Scripts/Others/camera.ts
@@ -1,21 +1,15 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property(cc.Node)
+    player: cc.Node = null;
 
-    @property
-    text: string = 'hello';
-
+    private playerX: number = 0;
+    private leftBound: number = -2454;
+    private rightBound: number = 2454;
+    
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
@@ -24,5 +18,16 @@ export default class NewClass extends cc.Component {
 
     }
 
-    // update (dt) {}
+    update (dt) {
+        if (this.player) {
+            if (this.player.x < this.leftBound) {
+                this.node.x = this.leftBound;
+            }
+            else if (this.player.x > this.rightBound) {
+                this.node.x = this.rightBound;
+            } else {
+                this.node.x = this.player.x;
+            }
+        }
+    }
 }
